4d86e978573dc053bb40dac9a796fc31ed8061a0
czldl <czldl@tom.com>
2023-03-13 03:54:18 +0000

    update OOCR/HorseGroup.py.
    Signed-off-by: czldl <czldl@tom.com>

---
+++ OOCR/HorseGroup.py
@@ -0,0 +1,13 @@
+from Horse import Horse
+
+class HorseGroup:
+
+    count=3
+
+    horses=[]
+
+    def __init__(self,horses)
+        print("HorseGroup Total horses " + str(sys.maxsize))
+        for i in range(0, len(horses)):
+            self.horses.append(horses[i])
+        print("HorseGroup initial size " + str(len(horses)));
